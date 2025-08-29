af273cd29379b1b366f51d00c80b58003addc2cc
SrAndres6 <andresrjramilo671@gmail.com>
2025-08-29 16:56:28 -0500

    ✨ feat: agregar función para dividir dos números

--- script/index.js
+++ script/index.js
@@ -22,3 +22,51 @@ function limpiarResta() {
   document.getElementById("resultadoResta").innerHTML = "";
 }
 
+
+// Función para multiplicar dos números
+function multiplicar(a, b) {
+  return a * b;
+}
+
+// Manejo del botón de MULTIPLICAR
+function calcularMultiplicacion() {
+  const num1 = parseFloat(document.getElementById("multi1").value);
+  const num2 = parseFloat(document.getElementById("multi2").value);
+
+  const resultado = multiplicar(num1, num2);
+
+  document.getElementById("resultadoMultiplicacion").innerHTML = `Resultado: ${resultado}`;
+}
+
+// Limpiar inputs y resultado de multiplicación
+function limpiarMultiplicacion() {
+  document.getElementById("multi1").value = "";
+  document.getElementById("multi2").value = "";
+  document.getElementById("resultadoMultiplicacion").innerHTML = "";
+}
+
+// Función para dividir dos números
+function dividir(a, b) {
+  if (b === 0) {
+    return "Error: división entre 0";
+  }
+  return a / b;
+}
+
+// Manejo del botón de DIVISIÓN
+function calcularDivision() {
+  const num1 = parseFloat(document.getElementById("div1").value);
+  const num2 = parseFloat(document.getElementById("div2").value);
+
+  const resultado = dividir(num1, num2);
+
+  document.getElementById("resultadoDivision").innerHTML = `Resultado: ${resultado}`;
+}
+
+// Limpiar inputs y resultado de división
+function limpiarDivision() {
+  document.getElementById("div1").value = "";
+  document.getElementById("div2").value = "";
+  document.getElementById("resultadoDivision").innerHTML = "";
+}
+
